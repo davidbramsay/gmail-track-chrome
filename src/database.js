7f77cd3426ed9046fbb4918f5e05426d0353d059
David Ramsay <davidramsay@davids-mbp-4.media.mit.edu>
2023-04-13 14:45:40 -0400

    lots of changes to get this working, added delete and last active done by hand

--- src/database.js
+++ src/database.js
@@ -8,141 +8,290 @@
 //each entry (timestamp).  This means we don't pull the same data object out of storage every
 //time, append, and rewrite.
 
+var Base64 = {
+
+    // private property
+    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+
+    // public method for encoding
+    , encode: function (input)
+    {
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = Base64._utf8_encode(input);
+
+        while (i < input.length)
+        {
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+
+            if (isNaN(chr2))
+            {
+                enc3 = enc4 = 64;
+            }
+            else if (isNaN(chr3))
+            {
+                enc4 = 64;
+            }
+
+            output = output +
+                this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+                this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+        } // Whend 
+
+        return output;
+    } // End Function encode 
 
 
+    // public method for decoding
+    ,decode: function (input)
+    {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
 
-//load + load_meta
-//poll, refresh + load_meta
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+        while (i < input.length)
+        {
+            enc1 = this._keyStr.indexOf(input.charAt(i++));
+            enc2 = this._keyStr.indexOf(input.charAt(i++));
+            enc3 = this._keyStr.indexOf(input.charAt(i++));
+            enc4 = this._keyStr.indexOf(input.charAt(i++));
 
-//open + email_meta
-//reply open + email_meta
-//save_draft, discard_draft + sending email_meta
-//reply send + sending email_meta
-//delete + email_meta
-//mark unread, mark read, star, unstar, mark important, mark not important, move folder + email_meta
-//
-//email meta = subject, body, to/from, bcc/cc, # in thread, date
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+
+            output = output + String.fromCharCode(chr1);
+
+            if (enc3 != 64)
+            {
+                output = output + String.fromCharCode(chr2);
+            }
+
+            if (enc4 != 64)
+            {
+                output = output + String.fromCharCode(chr3);
+            }
+
+        } // Whend 
+
+        output = Base64._utf8_decode(output);
+
+        return output;
+    } // End Function decode 
 
 
-// [datetime, action, {email_meta OR load_meta}]
+    // private method for UTF-8 encoding
+    ,_utf8_encode: function (string)
+    {
+        var utftext = "";
+        string = string.replace(/\r\n/g, "\n");
 
+        for (var n = 0; n < string.length; n++)
+        {
+            var c = string.charCodeAt(n);
+
+            if (c < 128)
+            {
+                utftext += String.fromCharCode(c);
+            }
+            else if ((c > 127) && (c < 2048))
+            {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else
+            {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+
+        } // Next n 
+
+        return utftext;
+    } // End Function _utf8_encode 
+
+    // private method for UTF-8 decoding
+    ,_utf8_decode: function (utftext)
+    {
+        var string = "";
+        var i = 0;
+        var c, c1, c2, c3;
+        c = c1 = c2 = 0;
+
+        while (i < utftext.length)
+        {
+            c = utftext.charCodeAt(i);
+
+            if (c < 128)
+            {
+                string += String.fromCharCode(c);
+                i++;
+            }
+            else if ((c > 191) && (c < 224))
+            {
+                c2 = utftext.charCodeAt(i + 1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            }
+            else
+            {
+                c2 = utftext.charCodeAt(i + 1);
+                c3 = utftext.charCodeAt(i + 2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+
+        } // Whend 
+
+        return string;
+    } // End Function _utf8_decode 
+
+}
 
 
 //WE NEED: write, read, download, clear
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
-class Database {
+    switch(message.call){
 
-    //get all the data in the database	
-    static get = () => {
-        const promise = toPromise((resolve, reject) => {
-            chrome.storage.local.get(null, (result) => {
-                if (chrome.runtime.lastError)
-                    reject(chrome.runtime.lastError);
-
-                resolve(result ?? []);
-            });
-        });
-
-        return promise;
-    }
-
-    //clear the database	
-    static clear = () => {
-        const promise = toPromise((resolve, reject) => {
+	  case 'clear':
             chrome.storage.local.clear(() => {
                 if (chrome.runtime.lastError)
-                    reject(chrome.runtime.lastError);
-                resolve(true);
+                    sendResponse(chrome.runtime.lastError);
+                sendResponse(true);
             });
-        });
+	  break;
 
-        return promise;
-    }
+	  case 'get':
+            chrome.storage.local.get(null, (result) => {
+                if (chrome.runtime.lastError)
+                    sendResponse(chrome.runtime.lastError);
 
-    //write to the database	
-    static write = (eventType, data) => {
-	console.log('called write');    
+                sendResponse(result ?? []);
+            });
+	  break;
 
-        const promise = toPromise((resolve, reject) => {
+	  case 'download':
+            chrome.storage.local.get(null, (result) => {
+                if (chrome.runtime.lastError)
+                    sendResponse(chrome.runtime.lastError);
+
+                let data = (result ?? []);
+		let url = 'data:application/json;base64,' + Base64.encode(JSON.stringify(data)); //get ready for writing   
+
+		let tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds	
+            	let ts = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
+
+	    	let filename = ts.substring(5,7) + ts.substring(8,10) + ts.substring(2,4) + '_';
+	    	filename += ts.substring(11,13) + ts.substring(14,16) + ts.substring(17,19) + '_email.json';
+
+            	chrome.downloads.download({'url': url,'filename': filename}, (downloadId) => {
+                	if (downloadId === undefined) 
+			    sendResponse(chrome.runtime.lastError);
+
+			chrome.storage.local.clear(() => {
+			    if (chrome.runtime.lastError)
+			        sendResponse(chrome.runtime.lastError);
+			    sendResponse(true);
+			});
+	        });
+            });
+	  break;
+
+	  case 'write':
+
 	    let tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds	
             let ts = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
-            chrome.storage.local.set({[ts]: {'e': eventType, 'data':data}}, () => {
+
+            chrome.storage.local.set({[ts]: {'e': message.eventType, 'data':message.data}}, () => {
                 if (chrome.runtime.lastError)
-                    reject(chrome.runtime.lastError);
-                resolve(true);
+                    sendResponse(chrome.runtime.lastError);
+                sendResponse(true);
             });
-        });
+	  break;		
 
-        return promise;
-    }
+  }
 
-    //download database and clear if true passed
-    static download = async (clear=false) => {
-	console.log('called download');    
+  return true;	
 
-        let data = await this.get(); //get all data  
-    	let url = 'data:application/json;base64,' + btoa(JSON.stringify(data)); //get ready for writing   
+});
 
-        const promise = toPromise((resolve, reject) => {
 
-            let tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds	
+chrome.runtime.onMessageExternal.addListener((message, sender, sendResponse) => {
+
+    switch(message.call){
+
+	  case 'clear':
+            chrome.storage.local.clear(() => {
+                if (chrome.runtime.lastError)
+                    sendResponse(chrome.runtime.lastError);
+                sendResponse(true);
+            });
+	  break;
+
+	  case 'get':
+            chrome.storage.local.get(null, (result) => {
+                if (chrome.runtime.lastError)
+                    sendResponse(chrome.runtime.lastError);
+
+                sendResponse(result ?? []);
+            });
+	  break;
+
+	  case 'download':
+            chrome.storage.local.get(null, (result) => {
+                if (chrome.runtime.lastError)
+                    sendResponse(chrome.runtime.lastError);
+
+                let data = (result ?? []);
+		let url = 'data:application/json;base64,' + btoa(JSON.stringify(data)); //get ready for writing
+
+		let tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+            	let ts = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
+
+	    	let filename = ts.substring(5,7) + ts.substring(8,10) + ts.substring(2,4) + '_';
+	    	filename += ts.substring(11,13) + ts.substring(14,16) + ts.substring(17,19) + '_email.json';
+
+            	chrome.downloads.download({'url': url,'filename': filename}, (downloadId) => {
+                	if (downloadId === undefined)
+			    sendResponse(chrome.runtime.lastError);
+
+			chrome.storage.local.clear(() => {
+			    if (chrome.runtime.lastError)
+			        sendResponse(chrome.runtime.lastError);
+			    sendResponse(true);
+			});
+	        });
+            });
+	  break;
+
+	  case 'write':
+
+	    let tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
             let ts = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
 
-	    let filename = ts.substring(5,7) + ts.substring(8,10) + ts.substring(2,4) + '_';
-	    filename += ts.substring(11,13) + ts.substring(14,16) + ts.substring(17,19) + '_email.json';
-
-            chrome.downloads.download({'url': url,'filename': filename}, (downloadId) => {
-                if (downloadId === undefined) 
-                    reject(chrome.runtime.lastError);
-
-		if (clear){
-		  return this.clear(resolve, reject);	
-		} else {
-		  resolve(true);
-		}
+            chrome.storage.local.set({[ts]: {'e': message.eventType, 'data':message.data}}, () => {
+                if (chrome.runtime.lastError)
+                    sendResponse(chrome.runtime.lastError);
+                sendResponse(true);
             });
-        });
+	  break;
 
-        return promise;
-    }
-}
+  }
 
+  return true;
 
-const toPromise = (callback) => {
-    const promise = new Promise((resolve, reject) => {
-        try {
-            callback(resolve, reject);
-        }
-        catch (err) {
-            reject(err);
-        }
-    });
-    return promise;
-}
-
-
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	switch(message.call){
-	  case 'clear':
-		Database.clear().then(() => {
-      			sendResponse(true);
-    		}, ()=> { sendResponse(false)});
-		break;
-	  case 'download':
-		Database.download(true).then((results) => {
-      			sendResponse(true);
-    		}, ()=> { sendResponse(false)});
-		break;
-	  case 'get':
-		Database.get().then((results) => {
-      			sendResponse(results);
-    		}, ()=> { sendResponse(false)});
-		break;
-	  case 'write':
-		Database.write(message.eventType, message.data).then(() => {
-      			sendResponse(true);
-    		}, ()=> { sendResponse(false)});
-	}
-	return true;	
 });
+
--- src/extension.js
+++ src/extension.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 // loader-code: wait until gmailjs has finished loading, before triggering actual extensiode-code.
 const loaderId = setInterval(() => {
     if (!window._gmailjs) {
@@ -11,37 +12,157 @@ const loaderId = setInterval(() => {
 }, 100);
 
 
+//load + load_meta
+//poll, refresh + load_meta
+
+//open + email_meta
+//reply open + email_meta
+//save_draft, discard_draft + sending email_meta
+//reply send + sending email_meta
+//delete + email_meta
+//mark unread, mark read, star, unstar, mark important, mark not important, move folder + email_meta
+//
+//email meta = subject, body, to/from, bcc/cc, # in thread, date
+
+
 function sendData(eventType, data){
+
 	var sendData = {};
 
 	sendData['call'] = 'write';
 	sendData['eventType'] = eventType;
 	sendData['data'] = data;
 
-	chrome.runtime.sendMessage(sendData, (resp) => {
+	console.log('----' + eventType + '----');
+	console.log(data);
+
+	chrome.runtime.sendMessage('bhhogfkjnaikemfloacacpcikllmhndl', sendData, (resp) => {
 		if (!resp){ console.error('SEND_DATA FAILED.'); }
 	});
 }
+
+function sendLoad(e){
+        const userEmail = gmail.get.user_email();
+
+	let unread={};
+	try{
+		let targ = document.querySelectorAll('[role="navigation"]')[0];
+		let els = targ.querySelectorAll("[aria-label]");
+		for (let i in els){
+		   try{	
+			var label = els[i].textContent;	
+			var number = parseInt(els[i].parentElement.parentElement.getElementsByTagName('div')[0].textContent);
+			unread[label] = number;	
+		   }catch(e){
+			console.error(e);
+		   }
+		}
+	}catch(e){
+		console.error(e);
+	}
+
+	let lastActive = '';
+	try{
+		let targ = document.querySelectorAll('[role="contentinfo"]')[0];
+		let els = targ.querySelectorAll('div:not(:has(*))');
+		for (let i in els){
+		   try{	
+			let str = els[i].textContent;
+			if(str.includes('Last account activity:')){
+
+				console.log(str);   
+				lastActive = str.replace('Last account activity:','').trim();
+				break;
+			}
+		   }catch(e){
+			console.error(e);
+		   }
+		}
+	}catch(e){
+		console.error(e);
+	}
+
+
+	sendData(e, {
+		'userEmail': userEmail, 
+		'unread': unread,
+		'lastActive': lastActive 
+	});    
+
+}
+
 
 // actual extension-code
 function startExtension(gmail) {
     console.log("Extension loading...");
     window.gmail = gmail;
 
+    gmail.observe.on("poll", () => { sendLoad('poll'); });
+    gmail.observe.on("refresh", () => { sendLoad('refresh'); });
     gmail.observe.on("load", () => {
-        const userEmail = gmail.get.user_email();
-        console.log("Hello, " + userEmail + ". This is your extension talking!");
 
-	sendData('loaded', {'userEmail': userEmail});    
+	setTimeout(() => {sendLoad('load');}, 500);
+
+	document.addEventListener('click', function(e) {
+	    e = e || window.event;
+	    var target = e.target || e.srcElement;
+
+	    //if delete event	
+	    if (target.outerHTML.includes('data-tooltip="Delete"')){
+
+		    console.log('delete event');
+
+		    //get parent parent 'tr' and then all spans inside, pull out text
+	    	    let spans = target.closest('tr').closest('tr').getElementsByTagName("span");
+
+		    let spanSet = new Set();
+	    	    for (let s in spans){
+		      spanSet.add(spans[s].textContent);
+		    }	
+
+	    	    //turn that to data and send
+	    	    sendData('delete', {'text': Array.from(spanSet)}); 
+	    }
+
+
+	}, false);
 
         gmail.observe.on("view_email", (domEmail) => {
-            console.log("Looking at email:", domEmail);
-            const emailData = gmail.new.get.email_data(domEmail);
-            console.log("Email data:", emailData);
+
+            var emailData = gmail.new.get.email_data(domEmail);
+	    emailData['content_html'] = emailData["content_html"].replace(/<[^>]+>/g, ' ');
+
+	    sendData("view_email", emailData);	
         });
 
+        gmail.observe.on("send_message", (url, body, data, xhr) => {
+
+	    var temp_body = body.replace(/<[^>]+>/g, ' ');
+
+	    sendData("send_message", {
+		'data':data,
+		'url':url,
+		'body':temp_body,
+		'xhr':xhr    
+	    });	
+        });
+
+
+
+
+//reply open + email_meta
+//save_draft, discard_draft + sending email_meta
+//reply send + sending email_meta
+//delete + email_meta
+//mark unread, mark read, star, unstar, mark important, mark not important, move folder + email_meta
+
         gmail.observe.on("compose", (compose) => {
-            console.log("New compose window is opened!", compose);
+            sendData("compose_open", {});
+        });
+        gmail.observe.on("compose_cancelled", (compose) => {
+            sendData("compose_cancelled", {});
         });
     });
+
+  		
 }
